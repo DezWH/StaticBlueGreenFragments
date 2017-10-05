88271fc76a59c33aa9ce69dbf112ea19f54e719d
D.W <Fake@email.com>
2017-10-05 01:49:50 -0500

    I'm going into the Gradle .build but I'm not sure on what has to be changed.

--- app/src/main/java/com/dez/staticbluegreenfragments/BlueFragment.java
+++ app/src/main/java/com/dez/staticbluegreenfragments/BlueFragment.java
@@ -4,15 +4,60 @@ package com.dez.staticbluegreenfragments;
  * Created by danne on 9/16/2017.
  */
 
-import android.app.Fragment;
+//import android.support.v4.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class BlueFragment extends Fragment {
+
+    //To save a refernce to something that can receive a random number, so can call method to send data
+     RandomNumberGeneratedListener randomListener;
+
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.blue_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.blue_fragment, container, false);
+
+        Button sendRndToGreen = (Button) view.findViewById(R.id.send_rnd_to_green_button);
+        sendRndToGreen.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Generate a random number and send it to the RandomNumberGeneratedListener.
+                //This Fragment doesn't know or care what happens to the random number.
+                Random rng = new Random();
+                int rnd = rng.nextInt(100);   //Random number between 0 and 99
+                randomListener.sendRandomNumber(rnd);
+
+            }
+
+        });
+
+        return view;
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        //Typically, hte listener is the hosting Activiity. It doens't have ty, but usually is.
+        if (getActivity() instanceof RandomNumberGeneratedListener) {
+            randomListener = (RandomNumberGeneratedListener) getActivity();
+        } else {
+            throw new RuntimeException(getActivity().getClass().toString() + "should implement RandomNumberGeneratedListener");
+            //hostingActivity = (MainActivity) getActivity();
+        }
+
+    }
+
+    interface RandomNumberGeneratedListener {
+        void sendRandomNumber (int rnd);
+    }
+
 }
--- app/src/main/java/com/dez/staticbluegreenfragments/GreenFragment.java
+++ app/src/main/java/com/dez/staticbluegreenfragments/GreenFragment.java
@@ -4,17 +4,38 @@ package com.dez.staticbluegreenfragments;
  * Created by danne on 9/16/2017.
  */
 
+//import android.app.Fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class GreenFragment extends Fragment{
+public class GreenFragment extends Fragment {
 
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.green_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.green_fragment, container, false);
+
+        int random = -1;
+
+        //Check to seee if argument have been received - if so, get random nmber
+        if (getArguments() != null) {
+            random = getArguments().getInt(MainActivity.RANDOM_BUNDLE_KEY, -1); //default for no random number
+        }
+        TextView showRandomTV = (TextView) view.findViewById(R.id.green_fragment_random_textview);
+
+            if (random == -1) {
+                //No random number received, no arguments or no data for RANDOM_BUNDLE_KEY
+                showRandomTV.setText("No random number received :(");
+            } else {
+                showRandomTV.setText("The random number is" + random);
+            }
+            return view;
+
+        }
     }
 
-}
+
+
