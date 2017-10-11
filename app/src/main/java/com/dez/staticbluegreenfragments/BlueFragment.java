ee9e3871f3ef4ccdd3662bb441190818ee2d08a9
D.W <Fake@email.com>
2017-10-11 12:58:05 -0500

    Completed Static Blue

--- app/src/main/java/com/dez/staticbluegreenfragments/BlueFragment.java
+++ app/src/main/java/com/dez/staticbluegreenfragments/BlueFragment.java
@@ -6,6 +6,7 @@ package com.dez.staticbluegreenfragments;
 
 //import android.support.v4.Fragment;
 import android.content.Context;
+
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,13 +16,23 @@ import android.widget.Button;
 
 import java.util.Random;
 
+
+
+/* For generating a random number */
+
+
+
 public class BlueFragment extends Fragment {
 
-    //To save a refernce to something that can receive a random number, so can call method to send data
-     RandomNumberGeneratedListener randomListener;
+
+
+    //To save a reference to something that can receive a random number, so can call methods to send data
+
+    RandomNumberGeneratedListener randomListener;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
 
         View view = inflater.inflate(R.layout.blue_fragment, container, false);
 
@@ -30,8 +41,11 @@ public class BlueFragment extends Fragment {
 
             @Override
             public void onClick(View v) {
+
                 //Generate a random number and send it to the RandomNumberGeneratedListener.
                 //This Fragment doesn't know or care what happens to the random number.
+
+                randomListener = (RandomNumberGeneratedListener) getActivity();
                 Random rng = new Random();
                 int rnd = rng.nextInt(100);   //Random number between 0 and 99
                 randomListener.sendRandomNumber(rnd);
@@ -41,23 +55,29 @@ public class BlueFragment extends Fragment {
         });
 
         return view;
+
     }
+
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        //Typically, hte listener is the hosting Activiity. It doens't have ty, but usually is.
-        if (getActivity() instanceof RandomNumberGeneratedListener) {
+        //Typically, the listener is the hosting Activity. It doesn't have to by, but usually is.
+        if ( getActivity() instanceof RandomNumberGeneratedListener) {
             randomListener = (RandomNumberGeneratedListener) getActivity();
         } else {
-            throw new RuntimeException(getActivity().getClass().toString() + "should implement RandomNumberGeneratedListener");
-            //hostingActivity = (MainActivity) getActivity();
+            throw new RuntimeException(getActivity().getClass().toString() + " should implement RandomNumberGeneratedListener");
+
         }
 
     }
 
     interface RandomNumberGeneratedListener {
-        void sendRandomNumber (int rnd);
+
+        void sendRandomNumber(int rnd);
+
     }
 
+
 }
+
